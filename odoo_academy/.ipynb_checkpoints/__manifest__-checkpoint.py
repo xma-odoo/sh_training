29461db23cfe9d80f755401a09fb3dad2df83548
Xueran(Sherry) <xma@odoo.com>
2021-05-25 22:07:04 +0000

    [ADD] Security and make views work

--- odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -15,6 +15,8 @@
     'version': '0.1',
     'depends': ['base'],
     'data': [
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
         'views/academy_menuitems.xml',
     ],
     'demo': [
