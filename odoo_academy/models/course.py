c8a624105cf5e5e63c28becce893391f0aecc038
Xueran(Sherry) <xma@odoo.com>
2021-05-26 21:22:38 +0000

    [ADD]ORM onchange, constrains, error management

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -2,6 +2,7 @@
 
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class Course(models.Model):
     
@@ -18,3 +19,24 @@ class Course(models.Model):
                              copy=False)
     
     active = fields.Boolean(string='Active', default=True)
+    
+    base_price = fields.Float(string='Base Price', default=0.00)
+    
+    additional_fee = fields.Float(string='Additional Fee', default=0.00)
+    
+    total_price = fields.Float(string='Total Price', readonly=True)
+    
+    @api.onchange('base_price', 'additional_fee')
+    def _onchange_total_price(self):
+        if self.base_price < 0.00:
+            raise UserError('Base Price cannot be set as Negative')
+            
+        self.total_price = self.base_price + self.additional_fee
+        
+    @api.constrains('additional_fee')
+    def _check_additional_fee(self):
+        for record in self:
+            if record.additional_fee < 10.00:
+                raise ValidationError('Additional Fees cannot be less than 10.00: %s' % record.additional_fee)
+    
+    
